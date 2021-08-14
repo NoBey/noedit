ee63749162f9df407cc0ff1dbccdff547bdcabb1
nobey <nobey@nobey.cn>
2021-08-15 02:33:43 +0800

    init

--- src/Editor.js
+++ src/Editor.js
@@ -1,7 +1,5 @@
 import keycode from "./keycode";
 
-
-
 class Editor {
   constructor(container) {
     this.el = document.createElement("div");
@@ -12,31 +10,36 @@ class Editor {
     this.el.style.overflowY = "scroll";
 
     this.el.setAttribute("contenteditable", true);
-    this.el.setAttribute("spellcheck", false); 
+    this.el.setAttribute("spellcheck", false);
     if (container) container.appendChild(this.el);
     this.init();
   }
 
   init() {
-      const { el } = this
-      el.addEventListener('keydown', this.onkeydown.bind(this))
-      el.addEventListener('keypress', this.onkeypress.bind(this))
-      el.addEventListener('keyup', this.onkeyup.bind(this))
-      el.addEventListener('input', this.oninput.bind(this))
+    const { el } = this;
+    el.addEventListener("keydown", this.onkeydown.bind(this), false);
+    el.addEventListener("keypress", this.onkeypress.bind(this));
+    el.addEventListener("keyup", this.onkeyup.bind(this));
+    el.addEventListener("input", this.oninput.bind(this));
   }
-  onkeydown(event){
-      console.log('onkeydown',String.fromCharCode(event.keyCode),event.keyCode)
+  onkeydown(event) {
+    event.stopPropagation()
+    console.log("onkeydown", String.fromCharCode(event.keyCode), event.keyCode);
+    // event.preventDefault()
   }
-  onkeypress(event){
-    console.log('onkeypress',String.fromCharCode(event.keyCode), event.keyCode)
+  onkeypress(event) {
+    console.log(
+      "onkeypress",
+      String.fromCharCode(event.keyCode),
+      event.keyCode
+    );
   }
-onkeyup(event){
-    console.log('onkeyup',String.fromCharCode(event.keyCode), event.keyCode)
-}
-oninput(event){
-    console.log('oninput',event)
-}
-
+  onkeyup(event) {
+    console.log("onkeyup", String.fromCharCode(event.keyCode), event.keyCode);
+  }
+  oninput(event) {
+    console.log("oninput", event);
+  }
 }
 
 export default Editor;
--- src/index.js
+++ src/index.js
@@ -10,5 +10,39 @@ document.body.appendChild(app)
 window.onload = () => {
 
    window.app =  new Editor(app)
-
+   window.app.el.innerHTML =  `
+   <h1>标题11</h1>
+   <h2>标题22</h2>
+   <h3>标题3</h3>
+   <p>阿花恢复肌肤健康反馈好呢</p>
+   `
 }
+
+function createTextArea(){
+  const textarea =  document.createElement('span')
+  textarea.setAttribute("contenteditable", true);
+  textarea.focus()
+  return textarea
+}
+
+let textarea = createTextArea()
+
+document.addEventListener('selectionchange', () => {
+   const selection = document.getSelection()
+   const range = selection.getRangeAt(0)
+   if(range.collapsed){
+      
+      const rightText = range.startContainer.splitText(range.startOffset)
+      rightText.parentNode.insertBefore(textarea, rightText)
+      textarea.innerHTML = "\u200B" 
+      range.setStart(textarea, 0);
+      range.setEnd(textarea, 1);
+      // range.selectNode(t )
+      selection.addRange(range)
+      // textarea.focus()
+      // console.log(rightText.insertBefore)
+      // rightText.insertBefore(createTextArea())   
+   }
+   
+   console.log(selection, range);
+ });
