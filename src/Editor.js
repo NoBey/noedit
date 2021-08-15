808fb6f35bfd920ba0d78b9c94f29a669cc435d2
nobey <nobey@nobey.cn>
2021-08-16 01:44:06 +0800

    init

--- src/Editor.js
+++ src/Editor.js
@@ -1,6 +1,65 @@
 import keycode from "./keycode";
 
+class Text {
+  constructor(text = "") {
+    this.node = document.createTextNode(text);
+  }
+  isText() {
+    return true;
+  }
+}
+
+class Paragraph {
+  child = [];
+  constructor(text) {
+    this.el = document.createElement("p");
+    if (text) {
+      this.child.push(new Text());
+    }
+  }
+}
+
+// class Model{
+//   child = []
+//   constructor(){
+//     this.child.push(new Paragraph)
+//   }
+
+// }
+
+function render(model = [], container) {
+  for (let i = 0; i < model.length; i++) {
+    model[i];
+    if (container) {
+      container.appendChild(model[i].el || model[i].node);
+    }
+  }
+}
+
+
+class Selection {
+  selection = window.getSelection()
+  range = null
+  
+  constructor(){
+    document.addEventListener('selectionchange', selectionchange.bind(this))
+  }
+
+  selectionchange(){
+   const selection = window.getSelection()
+   const range = selection.getRangeAt(0)
+   this.range = range
+   this.selection = selection
+  }
+}
+
+const selection = new Selection()
+
+// function in
+
 class Editor {
+  model = [];
+
   constructor(container) {
     this.el = document.createElement("div");
     this.el.style.width = "100%";
@@ -11,8 +70,14 @@ class Editor {
 
     this.el.setAttribute("contenteditable", true);
     this.el.setAttribute("spellcheck", false);
+
     if (container) container.appendChild(this.el);
+
+    this.model.push(new Paragraph());
     this.init();
+
+    render(this.model, this.el)
+
   }
 
   init() {
@@ -21,24 +86,35 @@ class Editor {
     el.addEventListener("keypress", this.onkeypress.bind(this));
     el.addEventListener("keyup", this.onkeyup.bind(this));
     el.addEventListener("input", this.oninput.bind(this));
+    el.addEventListener("beforeinput", this.beforeinput.bind(this));
   }
+
+  beforeinput(event) {
+    // console.log("beforeinput", event, event.isComposing);
+  }
+
   onkeydown(event) {
-    event.stopPropagation()
-    console.log("onkeydown", String.fromCharCode(event.keyCode), event.keyCode);
+    event.stopPropagation();
+    // console.log("onkeydown", String.fromCharCode(event.keyCode), event.keyCode);
     // event.preventDefault()
   }
+
   onkeypress(event) {
-    console.log(
-      "onkeypress",
-      String.fromCharCode(event.keyCode),
-      event.keyCode
-    );
+    // console.log(
+    //   "onkeypress",
+    //   String.fromCharCode(event.keyCode),
+    //   event.keyCode
+    // );
+    event.preventDefault();
   }
+
   onkeyup(event) {
-    console.log("onkeyup", String.fromCharCode(event.keyCode), event.keyCode);
+    // console.log("onkeyup", String.fromCharCode(event.keyCode), event.keyCode);
+    console.log(window.getSelection().getRangeAt(0));
   }
+
   oninput(event) {
-    console.log("oninput", event);
+    // console.log("oninput", event);
   }
 }
 
--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 const md = require('markdown').markdown
+
+let text = `
+# 欢迎使用 Cmd Markdown 编辑阅读器
+# 323
+
+`
+
+
+
+console.time(1)
+var tree = md.parse( text );
+console.timeEnd(1)
+console.log(tree)
